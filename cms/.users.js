d74a40e7b3f61eee2bedcf4d45a996587465df6e
Jason Davis <jasondavis3@gmail.com>
2017-03-13 22:36:05 -0700

    init

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'testadmin',
+			tags: [],
+			salt: '882c38235835721fd29a54eb060c2a4e',
+			hash: 'bd05a70bc0637770e4f2f1bbca84abd895f35977bd316f82be7979e959fe7627',
+			user_created_timestamp: 1489467668981
+		}
+	]
+}
